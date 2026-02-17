69abcbd770ea8e564416cc08b94d8cc712ee1a68
Apoorvasankuri <apoorvasankuri@gmail.com>
2026-02-17 15:00:48 +0530

    Update llm_processor_production.py

--- llm_processor_production.py
+++ llm_processor_production.py
@@ -115,6 +115,7 @@ def save_to_processed_articles(df: pd.DataFrame):
             published_date,
             news_title,
             link,
+            Source,
             relevance_score,
             competitor_tagging,
             sbu_tagging,
